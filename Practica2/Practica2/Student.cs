977fadf1cd2e7e6b5afb5c0d2d53495806fc6cd9
Gabriel <gabogabygpp16@gmail.com>
2021-04-11 13:53:55 -0400

    P2-003 Added file StudentsController.cs and Updated file Student.cs

--- Practica2/Practica2/Student.cs
+++ Practica2/Practica2/Student.cs
@@ -4,6 +4,7 @@ namespace Practica2
 {
     public class Student
     {
-     public String Name { get; set; }
+        public String Name { get; set; }
+        public String LastName { get; set; }
     }
 }
