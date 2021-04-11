90932c19274233c9536206f04d86c75ae4fca0b8
Gabriel <gabogabygpp16@gmail.com>
2021-04-11 13:12:05 -0400

    P2-002 Implemented file Student.cs

--- Practica2/Practica2/Controllers/WeatherForecastController.cs
+++ Practica2/Practica2/Controllers/WeatherForecastController.cs
@@ -24,10 +24,10 @@ namespace Practica2.Controllers
         }
 
         [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        public IEnumerable<Student> Get()
         {
             var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            return Enumerable.Range(1, 5).Select(index => new Student
             {
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = rng.Next(-20, 55),
